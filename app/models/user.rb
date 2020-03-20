a07d90d8773908ddb21a976cdd8adae911ef7769
ryo-iseki <61146134+ryo-iseki@users.noreply.github.com>
2020-03-20 20:42:47 +0900

    ユーザー登録機能、ログインログアウト機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,20 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable,
+         :validatable, password_length: 7..128
+  VALID_EMAIL_REGEX =  /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX},
+                    uniqueness: { case_sensitive: false }
+  validates :nickname, presence: true, length: { maximum: 50 }
+  validates :password, presence: true
+  validates :first_name, presence: true, format: {with: /\A[一-龥ぁ-ん]/}
+  validates :last_name, presence: true, format: {with: /\A[一-龥ぁ-ん]/}
+  validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
+  validates :last_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
+  validates :birth_date, presence: true
+  VALID_PHONE_REGEX = /\A\d{10}$|^\d{11}\z/
+  validates :tel, presence: true, format: {with: VALID_PHONE_REGEX}
+  has_many :items
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_20_060142) do
+ActiveRecord::Schema.define(version: 2020_03_20_110451) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -39,7 +39,7 @@ ActiveRecord::Schema.define(version: 2020_03_20_060142) do
     t.string "first_name_kana", null: false
     t.string "last_name_kana", null: false
     t.date "birth_date", null: false
-    t.integer "tel", null: false
+    t.string "tel", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
