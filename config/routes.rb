2048d0cea855fa8df3926942657a44c9c151e3a9
chikuwa0625 <57427219+chikuwa0625@users.noreply.github.com>
2020-03-26 14:12:37 +0900

    [マークアップ]　商品購入確認ページ

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root to: 'items#index'
   resources :items, only: [:index, :new, :create]
   resources :addressinfos, only: [:new, :create]
+  resources :purchases, only: [:new]
 end
