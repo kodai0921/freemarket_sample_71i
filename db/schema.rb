59fd54969b9e16e35f07febe43a5e9e18a8856eb
kodai0921 <k.kitahara51@gmail.com>
2020-03-29 17:12:34 +0900

    Revert "Revert "Revert "Revert "Merge pull request #13 from kodai0921/カテゴリー機能""""
    This reverts commit 0f6886f921dc485d26d324f8ff9329ecc0134d84.

--- db/schema.rb
+++ db/schema.rb
@@ -12,32 +12,14 @@
 
 ActiveRecord::Schema.define(version: 2020_03_29_013358) do
 
-  create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
+  create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "prefecture_id"
+    t.string "city"
     t.datetime "created_at", null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
-  end
-
-  create_table "active_storage_blobs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.bigint "byte_size", null: false
-    t.string "checksum", null: false
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+    t.datetime "updated_at", null: false
   end
 
   create_table "addressinfos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "first_name", null: false
-    t.string "last_name", null: false
-    t.string "first_name_kana", null: false
-    t.string "last_name_kana", null: false
     t.string "postcode", null: false
     t.integer "prefecture_code", null: false
     t.string "address_city", null: false
@@ -47,41 +29,63 @@ ActiveRecord::Schema.define(version: 2020_03_29_013358) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "zipcode"
     t.index ["user_id"], name: "index_addressinfos_on_user_id"
   end
 
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "ancestry"
+    t.string "name"
+    t.index ["ancestry"], name: "index_categories_on_ancestry"
+  end
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "text", null: false
+    t.integer "item_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "src", default: "", null: false
-    t.bigint "item_id", null: false
+    t.string "src"
+    t.bigint "item_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["item_id"], name: "index_images_on_item_id"
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", default: "", null: false
+    t.string "name", null: false
     t.text "version", null: false
-    t.integer "size"
+    t.string "brand"
+    t.string "condition", null: false
+    t.string "delivery_select", null: false
+    t.string "delivery_method_include_fee", null: false
+    t.string "delivery_method_COD", null: false
+    t.string "supplier_region", null: false
+    t.string "delivery_date", null: false
+    t.integer "saler_id"
+    t.integer "buyer_id"
+    t.integer "category_id"
+    t.integer "size", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "price"
     t.integer "condition_id"
-    t.string "brand"
     t.integer "ship_method_id"
     t.integer "prefecture_id"
     t.integer "delivery_date_id"
-    t.integer "buyer_id"
-    t.integer "saler_id"
   end
 
   create_table "pays", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "customer_id", null: false
+    t.string "card_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "card_number", null: false
-    t.integer "month", null: false
-    t.integer "day", null: false
-    t.integer "security_code", null: false
-    t.integer "user_id"
+    t.index ["user_id"], name: "index_pays_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -99,11 +103,16 @@ ActiveRecord::Schema.define(version: 2020_03_29_013358) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "postcode"
+    t.integer "prefecture_code"
+    t.string "address_city"
+    t.string "address_street"
+    t.string "address_building"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "addressinfos", "users"
   add_foreign_key "images", "items"
+  add_foreign_key "pays", "users"
 end
