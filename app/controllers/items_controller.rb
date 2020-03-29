1d406f530e890adac2dc5fa087a75723dbc5bb8e
kodai0921 <k.kitahara51@gmail.com>
2020-03-29 18:06:45 +0900

    organized

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,6 +22,7 @@ class ItemsController < ApplicationController
 
   def create
     if Item.create(item_params).save!
+      # @category_parent_array.create
       redirect_to  :root
     else
       @items = Item.new
@@ -36,7 +37,7 @@ class ItemsController < ApplicationController
   private
   def item_params
     params.require(:item).permit(:name, :version, :saler_id, :byer_id, :size, 
-      :price, :condition_id, :brand, :ship_method_id, :prefecture_id, :delivery_date_id,
+      :price, :condition_id, :brand, :ship_method_id, :prefecture_id, :delivery_date_id, :category_id,
       images_attributes: [:src]).merge(saler_id: current_user.id)
   end
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -6,6 +6,7 @@ class Item < ApplicationRecord
   belongs_to_active_hash :delivery_date
   belongs_to :saler, class_name: "User"
   belongs_to :buyer, class_name: "User", optional: true
+  belongs_to :category
   has_many :images
   accepts_nested_attributes_for :images, allow_destroy: true
   validates_associated :images
