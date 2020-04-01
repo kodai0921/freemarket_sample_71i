9ea0c12b84431a99711c3ecda0dfcab05ce44bf7
ryo-iseki <61146134+ryo-iseki@users.noreply.github.com>
2020-04-01 23:08:52 +0900

    購入画面にクレカ表示

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -9,6 +9,8 @@ class PurchasesController < ApplicationController
       #登録された情報がない場合にカード登録画面に移動
       redirect_to controller: "pays", action: "new"
     else
+      @user = current_user
+      @creditcard = Pay.where(user_id: current_user.id).first
       Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
       #保管した顧客IDでpayjpから情報取得
       customer = Payjp::Customer.retrieve(card.customer_id)
