60479eec5409065b4d1d2f956ef09ce2c98ad5f9
tsuboyama09 <61174442+tsuboyama09@users.noreply.github.com>
2020-04-03 19:34:39 +0900

    デプロイ作業

--- db/schema.rb
+++ db/schema.rb
@@ -65,7 +65,6 @@ ActiveRecord::Schema.define(version: 2020_04_02_103400) do
     t.integer "saler_id"
     t.integer "buyer_id"
     t.integer "category_id"
-    t.integer "size"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "condition_id"
